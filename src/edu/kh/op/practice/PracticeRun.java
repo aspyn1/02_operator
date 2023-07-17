90153adc7a3cf084aa09ef2d9543bc6e84e8dbea
aspyn1 <asypnisgood@gamil.com>
2023-07-17 17:51:51 +0900

    first commit

---
+++ src/edu/kh/op/practice/PracticeRun.java
@@ -0,0 +1,21 @@
+package edu.kh.op.practice;
+
+public class PracticeRun {
+	
+	public static void main(String[] args) {
+		
+		OperatorPractice op = new OperatorPractice();
+		
+		op.ex1();
+		op.ex2();
+		op.ex3();
+		
+	}
+	
+	
+	
+	
+
+
+
+}
---
+++ src/eud/kh/op/ex/ExampleRun.java
@@ -0,0 +1,26 @@
+package eud.kh.op.ex;
+
+public class ExampleRun { // 코드 실행용 클래스
+	
+	// 메인메서드 필수 작성
+	public static void main(String[] args) {
+		
+		// OpExample 생성
+		OpExample ex = new OpExample();
+		// 같은 패키지(eud.kh.op.ex)안에 있는 클래스는
+		// import를 하지 않아도 불러다 사용가능하다
+		// 반대로, Scanner 클래스는 패키지 내에 없기 때문에 import가 필요함
+		// Scanner sc = new Scanner();
+		
+		// ex.ex1(); // ex(new OpExample 클래스)가 가지고있는 ex1() 메서드 실행
+		// ex.ex2();
+		// ex.ex3();
+		ex.ex4();
+		// ex.ex5();
+		// ex.ex6();
+		// ex.ex7();
+		// ex.ex8();
+		
+	}
+
+}
